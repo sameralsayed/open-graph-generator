f2fdb481d524053cf534ee8866e5ca9738c4d912
Samer Saeid <74488921+sameralsayed@users.noreply.github.com>
2022-07-16 10:53:16 -0700

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+    #Name : Open Graph Generator
+    #Created by : SAMER SAEID
+    #Copyright : SAMER SAEID
+    #URL: https://www.facebook.com/samerthehariri/
+    #Date: 11/19/2021
+    
+echo "Site Name"
+read SiteName
+
+echo "Title:"
+read SiteTitle
+
+echo "Description (Characters left: 194):"
+read Description
+
+echo "Site URL:"
+read SiteURL
+
+echo "Image URL"
+read Image
+
+echo "<meta property=\"og:title\" content=\"${SiteTitle}\">" >> ${SiteName}.txt
+echo "<meta property=\"og:site_name\" content=\"${SiteName}\">" >> ${SiteName}.txt
+echo "<meta property=\"og:url\" content=\"${SiteURL}\">" >> ${SiteName}.txt
+echo "<meta property=\"og:description\" content=\"${Description}\">" >> ${SiteName}.txt
+echo "<meta property=\"og:image\" content=\"${Image}\">" >> ${SiteName}.txt
+echo "<meta property=\"og:type\" content=\"website\">"
